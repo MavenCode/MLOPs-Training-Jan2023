9c0fcf118f77339dc471ce70cb7df010d948e1bc
Charles Adetiloye <charles@mavencode.com>
2023-01-10 17:48:55 -0600

    url fix

--- Day2/1_creating_pipeline_components/b_reusable_component/instantiate_pipeline.py
+++ Day2/1_creating_pipeline_components/b_reusable_component/instantiate_pipeline.py
@@ -19,9 +19,11 @@ def preprocess_op():
 # exporting the component as a yaml file
 # TODO: fix this
 if __name__ == "__main__":
+
     kfp.components.create_component_from_func(
     preprocess_op, #function name
-    output_component_file="preprocess-reusable.yaml") ,
-    packages_to_install = ["pandas==0.23.4", "scikit-learn==0.22"]
+    output_component_file="preprocess-reusable.yaml" ,
+    packages_to_install = ["pandas==0.23.4", "scikit-learn==0.22"])
+                                        
 
 # view the output YAML file and cp it into `c_load_component_from_yaml_folder`
